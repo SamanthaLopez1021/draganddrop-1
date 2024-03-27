843630a73936b49e088b8e1c8c3c8e12970d2610
SamanthaLopez1021 <lopezeimmy402@gmail.com>
2024-03-27 12:45:44 -0500

    arreglo hexagonos

--- js/colmena.js
+++ js/colmena.js
@@ -1,20 +1,21 @@
 window.addEventListener("load",(e)=> {
-    let cordenadasiniciales = document.querySelector(".hexagono-base").getBoundingClientRect();
-    let cordenadasinicialesc = document.querySelector("#containerDrop_a").getBoundingClientRect();
+    let zonas = [];
     const elements = document.querySelectorAll(".hexagono");
-    // const containerDrop= document.getElementById("container_drop")
+    const containerDrag= document.querySelectorAll(".container-drag")
     const containerDrop= document.querySelectorAll(".container-drop")
+    
     elements.forEach(element => {
         element.addEventListener('dragstart',e => {
             // console.log("dragstart")
             e.dataTransfer.setData('id',e.target.id)
         });
         element.addEventListener('dragend',e => {
+        
             element.style.backgroundColor = ('#15CBB9');
             element.style.color=('white');
-            var imgService=element.querySelector('.sevices')
+            let imgService=element.querySelector('.sevices')
             imgService.style.display = 'none';
-            var containerDrop = document.getElementById("containerDrop_a");
+            let containerDrop = document.getElementById("containerDrop_a");
             containerDrop.style.borderRadius="50px 50px 0px 0px";
 
         });
@@ -24,6 +25,47 @@ window.addEventListener("load",(e)=> {
         });
         
     });
+
+    [0,1,2,3,4,5,6].forEach(n => {
+        zonas[n] = {
+            disponible: true,
+            elemento: "",
+            id:n
+        };
+    });
+    zonas[0].disponible = false
+
+
+    containerDrag.forEach(container=>{
+        
+        container.addEventListener('dragenter', e =>{
+        });
+        container.addEventListener('dragleave', e =>{
+            
+        });
+        container.addEventListener('dragover', e =>{
+            e.preventDefault();
+        });
+        container.addEventListener('drop', e =>{
+
+            let idElement = e.dataTransfer.getData('id')
+            let zonaAliberar=zonas.find( z => z.elemento===idElement);
+            if (zonaAliberar) {
+                zonas[zonaAliberar.id].disponible=true
+                zonas[zonaAliberar.id].elemento=""
+            }
+            
+            container.appendChild(document.getElementById(idElement));
+            
+            document.getElementById(idElement).style.position = 'relative';
+            document.getElementById(idElement).style.top ="";
+            document.getElementById(idElement).style.left ="";
+            document.getElementById(idElement).style.right ="";
+
+        });
+        
+    })
+    
     containerDrop.forEach(container=>{
         
         container.addEventListener('dragenter', e =>{
@@ -36,12 +78,11 @@ window.addEventListener("load",(e)=> {
             e.preventDefault();
         });
         container.addEventListener('drop', e =>{
-    
             
             let idElement = e.dataTransfer.getData('id')
-            let idElementproces = e.dataTransfer.getData('id').split("_")[1]
-            
-            let idBox=container.id.split("_")[1]
+            let zonaAocupar = obtenerZonaLibre(); //una funcion me da la zona a ocupar
+            zonas[zonaAocupar].disponible=false;
+            zonas[zonaAocupar].elemento=idElement;
             
 				//idElementproces === idBox  ? container.appendChild(document.getElementById(idElement)) : alert("no puedes colocar esto aqui")
 				container.appendChild(document.getElementById(idElement));
@@ -62,17 +103,18 @@ window.addEventListener("load",(e)=> {
 			})
             let tamañoPantalla = window.innerWidth;
             if(tamañoPantalla<=767){
-                let [num1,num2] =ubicacionPanalMobile(childs.length);
+                let [num1,num2] =ubicacionPanalMobile(zonaAocupar);
                 document.getElementById(idElement).style.position = "absolute";
                 document.getElementById(idElement).style.top =`${Math.floor(topcords + parseInt(num2))}px`
                 document.getElementById(idElement).style.left =`${Math.floor(rigthcord - parseInt(num1))}px`
                 document.getElementById(idElement).style.right =`${Math.floor(leftcord + parseInt(num1))}px`
             }else{
-            let [num1,num2] =ubicacionPanal(childs.length);
-            document.getElementById(idElement).style.position = "absolute";
-            document.getElementById(idElement).style.top =`${Math.floor(topcords + parseInt(num2))}px`
-            document.getElementById(idElement).style.left =`${Math.floor(rigthcord - parseInt(num1))}px`
-            document.getElementById(idElement).style.right =`${Math.floor(leftcord + parseInt(num1))}px`
+                
+                let [num1,num2] =ubicacionPanal(zonaAocupar);
+                document.getElementById(idElement).style.position = "absolute";
+                document.getElementById(idElement).style.top =`${Math.floor(topcords + parseInt(num2))}px`
+                document.getElementById(idElement).style.left =`${Math.floor(rigthcord - parseInt(num1))}px`
+                document.getElementById(idElement).style.right =`${Math.floor(leftcord + parseInt(num1))}px`
             }
         });
         
@@ -142,6 +184,10 @@ window.addEventListener("load",(e)=> {
             return [num1,num2] 
         }
         
+    }
+    function obtenerZonaLibre(){
+        const result = zonas.find( z => z.disponible);
+        return result.id
     }
 });
 
